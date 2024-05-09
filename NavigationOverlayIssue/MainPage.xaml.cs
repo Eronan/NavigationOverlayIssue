b94ddd5dd21c0e52a2f2b81428755318ad6a4c44
Ere <animeftw@live.co.uk>
2024-05-09 15:23:25 +1000

    Add project files.

---
+++ NavigationOverlayIssue/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace NavigationOverlayIssue;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (Navigation.NavigationStack.Count > 1)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+        else
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
+
